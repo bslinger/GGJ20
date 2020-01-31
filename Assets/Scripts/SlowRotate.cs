e57c1118190e4e581fe8b0880b4cbf42b3e64e80
Richard Gubb <richardbgubb@gmail.com>
2020-01-31 21:45:20 +1100

    Gyro test

---
+++ Assets/Scripts/SlowRotate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowRotate : MonoBehaviour
+{
+    bool rotating;
+    float speed = 0;
+
+    public void OnGUI()
+    {
+        if (!rotating)
+        {
+            GUILayout.Label("Press space to rotate");
+        } else
+        {
+            GUILayout.Label("Rotating at " + speed);
+            GUILayout.Label("Press space to stop rotation");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rotating = !rotating;
+        }
+        if (Input.GetKeyUp(KeyCode.KeypadPlus))
+        {
+            speed += 0.1f;
+        }
+        if (Input.GetKeyUp(KeyCode.KeypadMinus))
+        {
+            speed -= 0.1f;
+        }
+
+        if (rotating)
+        {
+            transform.Rotate(Vector3.forward * speed);
+        }
+    }
+}
