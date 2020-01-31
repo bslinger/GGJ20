f9d06c17f58c5b9d454a27313f5684efacfd8487
Ben Slinger <ben@slinger.com.au>
2020-01-31 23:29:35 +1100

    Scripts and prefabs for power core and connect points

---
+++ Assets/ConnectPoint.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectPoint : MonoBehaviour
+{
+
+    public GameObject ghostCylinderPrefab;
+
+    bool ghostShowing;
+    PowerCore connectedCore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerCore"))
+        {
+            // show ghost mode
+            Debug.Log("Ghost Mode On");
+            ghostCylinderPrefab.SetActive(true);
+            other.gameObject.GetComponent<PowerCore>().SetPointHovering(this);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerCore"))
+        {
+            Debug.Log("Ghost Mode Off");
+            ghostCylinderPrefab.SetActive(false);
+            other.gameObject.GetComponent<PowerCore>().SetPointHovering(null);
+        }
+    }
+
+    public void ConnectCore(PowerCore core)
+    {
+        connectedCore = core;
+        connectedCore.transform.position = transform.position;
+        connectedCore.transform.rotation = transform.rotation;
+        connectedCore.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        GetComponent<CapsuleCollider>().enabled = false;
+        ghostCylinderPrefab.SetActive(false);
+    }
+
+    public void DisconnectCore()
+    {
+        connectedCore.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        GetComponent<CapsuleCollider>().enabled = true;
+    }
+
+    public bool IsPowered()
+    {
+        if (connectedCore == null)
+        {
+            return false;
+        }
+        return connectedCore.powered;
+    }
+}
---
+++ Assets/PowerCore.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerCore : MonoBehaviour
+{
+
+    public ConnectPoint hoverPoint;
+    public ConnectPoint connectedPoint;
+
+    public bool powered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPointHovering(ConnectPoint point)
+    {
+        hoverPoint = point;
+    }
+
+    public void ConnectToPoint()
+    {
+        if (hoverPoint == null)
+        {
+            Debug.LogError("Tried to connect to point when not hovering");
+            return;
+        }
+        connectedPoint = hoverPoint;
+        hoverPoint = null;
+        
+        connectedPoint.ConnectCore(this);
+
+    }
+
+    public void OnDetachedFromHand()
+    {
+        Debug.Log("Detached from hand");
+        if (hoverPoint != null)
+        {
+            ConnectToPoint();
+        }
+    }
+
+    public void OnAttachedToHand()
+    {
+        if (connectedPoint != null)
+        {
+            connectedPoint.DisconnectCore();
+        }
+    }
+}
