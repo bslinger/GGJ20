31ce04447e2b9f59048d90135c1547e7c2da57f4
TrevorHScott <trevor.harvey.scott@gmail.com>
2020-02-01 22:08:50 +1100

    Intro scene animation and scene transition done (in the dodgiest way possible)

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] int waitTime;
+    [SerializeField] int fadeTime;
+    [SerializeField] GameObject player;
+
+
+    private void Start()
+    {
+        StartCoroutine(LoadGame());
+    }
+
+    IEnumerator LoadGame()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SteamVR_Fade.Start(Color.black, fadeTime);
+
+        yield return new WaitForSeconds(fadeTime);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Ben");
+        Destroy(player);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        
+        
+
+    }
+
+}
