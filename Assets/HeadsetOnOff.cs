77a1671cf59aa7a9e12773036a248f2c8ab748dc
TrevorHScott <trevor.harvey.scott@gmail.com>
2020-02-03 18:46:18 +1100

    Added splash screen to Attract mode, it disappears when headset is put on, and reappears when the headset is removed. Started work on providing a tutorial for the powercores.

---
+++ Assets/HeadsetOnOff.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+
+public class HeadsetOnOff : MonoBehaviour
+{
+    public SteamVR_Action_Boolean activeAction;
+    [SerializeField] SteamVR_Input_Sources input;
+    [SerializeField] Camera ExternalCamera;
+    [SerializeField] Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        activeAction.AddOnStateDownListener(this.HeadsetOn, input);
+        activeAction.AddOnStateUpListener(this.HeadsetOff, input);
+    }
+
+    void HeadsetOn(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        ExternalCamera.enabled = false;
+        canvas.enabled = false;
+    }
+
+    void HeadsetOff(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        ExternalCamera.enabled = true;
+        canvas.enabled = true;
+    }
+}
---
+++ Assets/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : SystemBase
+{
+
+    override protected void UpdateMe()
+    {
+        if (isPowered)
+        {
+            Decrease();
+        }
+        else
+        {
+            Increase();
+        }
+    }
+}
