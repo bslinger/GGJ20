84de52989c08a2a9a32f2d6b24c8440c1c7ac9c0
TrevorHScott <trevor.harvey.scott@gmail.com>
2020-02-03 23:43:58 +1100

    Attract mode now can start game via a console, and will fade music down before starting the original Intro scene.

--- Assets/HeadsetOnOff.cs
+++ Assets/HeadsetOnOff.cs
@@ -14,15 +14,23 @@ public class HeadsetOnOff : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (activeAction.GetActive(input))
+        {
+            enableHeadset();
+        }
         activeAction.AddOnStateDownListener(this.HeadsetOn, input);
         activeAction.AddOnStateUpListener(this.HeadsetOff, input);
     }
 
-    void HeadsetOn(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    void enableHeadset()
     {
         ExternalCamera.enabled = false;
         canvas.enabled = false;
+    }
+
+    void HeadsetOn(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        enableHeadset();
     }
 
     void HeadsetOff(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
