ef8325eeacf9057b83a1e0ecb7af30b9c4c7a7fc
TrevorHScott <trevor.harvey.scott@gmail.com>
2020-02-05 00:23:14 +1100

    Full loop appears to be working

---
+++ Assets/resetMusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetMusicPlayer : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            GameObject go = GameObject.FindGameObjectWithTag("music");
+            Destroy(go);
+        } catch { 
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
