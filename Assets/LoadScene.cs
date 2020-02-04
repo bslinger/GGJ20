a65c961d587dd6c6fb99d8420adea83663751533
TrevorHScott <trevor.harvey.scott@gmail.com>
2020-02-04 22:11:33 +1100

    Additional changes to powercore positioning and updates to LoadScene to allow for fadeout color changes

--- Assets/LoadScene.cs
+++ Assets/LoadScene.cs
@@ -10,24 +10,35 @@ public class LoadScene : MonoBehaviour
     [SerializeField] int fadeTime;
     [SerializeField] GameObject player;
     [SerializeField] string sceneToLoad = "Ben";
+    [SerializeField] Color color;
 
 
     private void Start()
     {
+        if (color == null) color = Color.black;
         StartCoroutine(LoadGame());
     }
 
-    public LoadScene(int waitTime, int fadeTime, string sceneToLoad)
+    public LoadScene(int waitTime, int fadeTime, string sceneToLoad, Color color = default)
     {
         this.waitTime = waitTime;
         this.fadeTime = fadeTime;
         this.sceneToLoad = sceneToLoad;
+        if(color == default)
+        {
+            this.color = Color.black;
+        }
+        else
+        {
+            this.color = color;
+        }
+
     }
 
     public IEnumerator LoadGame()
     {
         yield return new WaitForSeconds(waitTime);
-        SteamVR_Fade.Start(Color.black, fadeTime);
+        SteamVR_Fade.Start(color, fadeTime);
 
         yield return new WaitForSeconds(fadeTime);
 
