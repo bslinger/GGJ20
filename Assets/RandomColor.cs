0c9b417ca821f39ce5bb33566e51ae48b10f6b4a
TrevorHScott <trevor.harvey.scott@gmail.com>
2020-02-05 19:07:04 +1100

    New random color script added to the powercore prefab. Just cos

---
+++ Assets/RandomColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour
+{
+
+    [SerializeField] ParticleSystem particleSystem;
+    [SerializeField] Color[] colors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       var main = particleSystem.main;
+       main.startColor = colors[Random.Range(0, colors.Length - 1)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
