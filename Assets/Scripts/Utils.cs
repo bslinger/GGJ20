ad287b294b6172969bef5ee5f4f3ecc8bc5e37c3
Ben Slinger <ben@slinger.com.au>
2020-02-07 23:30:30 +1100

    Got lerp working properly when force pulling

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -7,13 +7,25 @@ public static class Utils
 {
    public static IEnumerator LerpThen(GameObject gameObject, Vector3 destination, float time, Action thenDo)
     {
+        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            rigidbody.isKinematic = true;
+        }
+        
         float elapsedTime = 0f;
         Vector3 startingPos = gameObject.transform.position;
+        Quaternion startingRot = gameObject.transform.rotation;
         while (elapsedTime < time)
         {
             gameObject.transform.position = Vector3.Lerp(startingPos, destination, (elapsedTime / time));
+            gameObject.transform.rotation = Quaternion.Lerp(startingRot, Quaternion.identity, (elapsedTime / time));
             elapsedTime += Time.deltaTime;
             yield return new WaitForEndOfFrame();
+        }
+        if (rigidbody != null)
+        {
+            rigidbody.isKinematic = false;
         }
         thenDo();
     }
