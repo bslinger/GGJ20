7deaa4d9665246085918513885bac5fa7915c5be
Ryan Stock <contact@ryanstock.com.au>
2020-02-13 12:21:21 +1100

    Various work on panel screens (animations, layouts)

--- Assets/Scripts/SystemBase.cs
+++ Assets/Scripts/SystemBase.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public abstract class SystemBase : MonoBehaviour
 {
@@ -9,18 +12,57 @@ public abstract class SystemBase : MonoBehaviour
     [SerializeField] protected float maxParameter;
     [SerializeField] protected float minParameter;
     [SerializeField] float step;
+    [FormerlySerializedAs("panelObject")] [SerializeField] GameObject panelEnabledObject;
+    [FormerlySerializedAs("panelDisableddObject")] [SerializeField] GameObject panelDisabledObject;
+    
     protected float currentParameter;
     protected bool isPowered;
     [SerializeField] GameObject powerSourceObject;
     PowerSource powerSource;
     AudioSource audioSource;
     protected DialogueManager dialogue;
+    
+    private Image panelEnabledObjectImage;
+    private Color panelEnabledObjectImageVisibleColor;
+    private Color panelEnabledObjectImageInvisibleColor;
+    
+    private Image panelDisabledObjectImage;
+    private Color panelDisabledObjectImageVisibleColor;
+    private Color panelDisabledObjectImageInvisibleColor;
+    
+    private bool animatingPanelColor = false;
+    private float panelTransitionPercent = 1;
 
     private void Start()
     {
+        if (panelEnabledObject)
+        {
+            panelEnabledObjectImage = panelEnabledObject.GetComponent<Image>();
+            panelEnabledObjectImageVisibleColor = panelEnabledObjectImageInvisibleColor = panelEnabledObjectImage.color;
+            panelEnabledObjectImageInvisibleColor.a = 0;
+        }
+        if (panelDisabledObject)
+        {
+            panelDisabledObjectImage = panelDisabledObject.GetComponent<Image>();
+            panelDisabledObjectImageVisibleColor = panelDisabledObjectImageInvisibleColor = panelDisabledObjectImage.color;
+            panelDisabledObjectImageInvisibleColor.a = 0;
+        }
+
         if (powerSourceObject)
         {
             powerSource = powerSourceObject.GetComponent<PowerSource>();
+            if (panelEnabledObjectImage)
+            {
+                panelEnabledObjectImage.color = powerSource.IsPowered()
+                    ? panelEnabledObjectImageVisibleColor
+                    : panelEnabledObjectImageInvisibleColor;
+            }
+            if (panelDisabledObjectImage)
+            {
+                panelDisabledObjectImage.color = powerSource.IsPowered()
+                    ? panelDisabledObjectImageInvisibleColor
+                    : panelDisabledObjectImageVisibleColor;
+            }
         }
         if (!powerSourceObject || powerSource == null)
         {
@@ -92,7 +134,42 @@ public abstract class SystemBase : MonoBehaviour
 
     void CheckPower()
     {
-        isPowered = powerSource.IsPowered();
+        const int transitionDuration = 1;
+        if (isPowered != powerSource.IsPowered()) // If there's been a change
+        {
+            isPowered = powerSource.IsPowered();
+            animatingPanelColor = true;
+            panelTransitionPercent = 1 - panelTransitionPercent;
+        } // "#00DAFF" : "#ABABAB"
+
+        if (animatingPanelColor)
+        {
+            panelTransitionPercent += Time.deltaTime / transitionDuration;
+            if (panelTransitionPercent >= 1)
+            {
+                animatingPanelColor = false;
+                panelTransitionPercent = 1;
+            }
+            if (panelEnabledObjectImage)
+            {
+                Debug.Log("Animating panelEnabledObjectImage.color");
+                panelEnabledObjectImage.color = Color.Lerp(
+                    isPowered ? panelEnabledObjectImageInvisibleColor : panelEnabledObjectImageVisibleColor,
+                    isPowered ? panelEnabledObjectImageVisibleColor : panelEnabledObjectImageInvisibleColor,
+                    panelTransitionPercent
+                );
+            }
+            if (panelDisabledObjectImage)
+            {
+                Debug.Log("Animating panelDisabledObjectImage.color");
+                panelDisabledObjectImage.color = Color.Lerp(
+                    isPowered ? panelDisabledObjectImageVisibleColor : panelDisabledObjectImageInvisibleColor,
+                    isPowered ? panelDisabledObjectImageInvisibleColor : panelDisabledObjectImageVisibleColor,
+                    panelTransitionPercent
+                );
+            }
+        }
+
         if (audioSource)
         {
             audioSource.enabled = isPowered;
